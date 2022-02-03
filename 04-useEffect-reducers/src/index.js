1afc1e0f621e648479b5c23f8e4297982a358a3e
Ronen <trr0123@gmail.com>
2022-02-03 14:13:19 +0200

    finished hooks section

---
+++ 04-useEffect-reducers/src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { AuthContextProvider } from "./store/auth-context";
+import "./index.css";
+import App from "./App";
+
+ReactDOM.render(
+  <AuthContextProvider>
+    <App />
+  </AuthContextProvider>,
+  document.getElementById("root")
+);
