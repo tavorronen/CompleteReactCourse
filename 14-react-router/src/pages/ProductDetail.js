830d1052f9fc896c2b8edd395ef9e6370a2d67bf
ronenbadalov <ronenbadalov1@gmail.com>
2022-03-15 22:47:22 +0200

    finished redux, started react router

---
+++ 14-react-router/src/pages/ProductDetail.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+const ProductDetail = () => {
+  const params = useParams();
+  console.log(params.productId);
+  return (
+    <section>
+      <h1>ProductDetail</h1>
+      <p>{params.productId}</p>
+    </section>
+  );
+};
+
+export default ProductDetail;
---
+++ 14-react-router/src/pages/Products.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Products = () => {
+  return (
+    <section>
+      <h1>The Products Page</h1>
+      <ul>
+        <li>A Book</li>
+        <li>A Carpet</li>
+        <li>An Online Course</li>
+      </ul>
+    </section>
+  );
+};
+
+export default Products;
