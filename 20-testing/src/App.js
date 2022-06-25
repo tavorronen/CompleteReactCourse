0e8c9a10c54cb38b577db4a497a96006a40a2e67
Ronen Badalov <ronenbadalov1@gmail.com>
2022-06-25 16:19:24 +0300

    .

---
+++ 20-testing/src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Greeting from "./components/Greeting";
+import Async from "./components/Async";
+
+function App() {
+  return (
+    <div className="App">
+      <Async />
+      <Greeting />
+    </div>
+  );
+}
+
+export default App;
